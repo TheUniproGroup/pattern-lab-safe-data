26345ddd3a0b95a650b0c573026ff75d78e45ade
Andy <3338309+andriokha@users.noreply.github.com>
2019-03-09 17:38:53 +0000

    Factor out reading the config

--- src/FabbDev/SafeData/PatternLabListener.php
+++ src/FabbDev/SafeData/PatternLabListener.php
@@ -8,14 +8,23 @@ use PatternLab\Listener;
 
 /**
  * Provides a Pattern Lab listener to prevent data values from being escaped.
+ *
+ * @todo Add tests.
+ * @todo Try with PSR-4.
+ * @todo Change from MakeSafe to MarkSafe - this doesn't transform anything.
  */
 class PatternLabListener extends Listener {
+
+  /**
+   * The plugin's name in the Pattern Lab configuration and composer.json.
+   */
+  protected const PLUGIN_NAME = 'safeData';
 
   /**
    * Create the listener and subscribe it to the data loaded event.
    */
   public function __construct() {
-    $this->addListener('patternData.dataLoaded','processSafeData');
+    $this->addListener('patternData.dataLoaded', 'processSafeData');
   }
 
   /**
@@ -25,13 +34,13 @@ class PatternLabListener extends Listener {
    * Twig.
    */
   public function processSafeData() {
-    if (!Config::getOption('plugins.safeData.enabled')) {
+    if (!$this->getConfig('enabled')) {
       return;
     }
 
     // Ideally we'd read the character set from the environment somehow. It
     // doesn't seem to be possible from the event object.
-    $charset = Config::getOption('plugins.safeData.charset') ?? 'UTF-8';
+    $charset = $this->getConfig('charset') ?? 'UTF-8';
 
     $data = Data::get();
     array_walk_recursive($data, function (&$value) use ($charset) {
@@ -45,6 +54,19 @@ class PatternLabListener extends Listener {
     });
 
     Data::replaceStore($data);
+  }
+
+  /**
+   * Returns the requested plugin configuration.
+   *
+   * @param string $name
+   *   The name of the config option in dotted form, eg. 'enabled', 'us.title'.
+   *
+   * @return string|false
+   *   The configuration value, or false if it wasn't found.
+   */
+  protected function getConfig($name) {
+    return Config::getOption('plugins.' . static::PLUGIN_NAME . ".$name");
   }
 
 }
